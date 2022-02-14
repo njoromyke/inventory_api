1c1943ebfb3af40dc01430e71584acc74492f054
ngahika01 <mykenjoroge01@gmail.com>
2022-02-14 20:40:52 +0300

    fixed jwttoken expiry time

--- utils/generateTokens.js
+++ utils/generateTokens.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 const generateToken = (id) =>{
     return jwt.sign({id},process.env.JWT_TOKEN,{
-        expiresIn: '30d'
+        expiresIn: '1h'
     })
 }
 export default generateToken
