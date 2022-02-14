24899744900e36632fc84aeb5330f56ba31f79a1
ngahika01 <mykenjoroge01@gmail.com>
2022-02-14 20:00:25 +0300

    Fixed protection of user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,13 +9,20 @@ import {
   updateUser,
   updateUserProfile,
 } from "../controllers/userControllers.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(registerUser).get(getUsers);
 router.route("/auth").post(authUser);
-router.route("/:id").get(getUserById).delete(deleteUser).put(updateUser);
-router.route("/profile/:id").get(getUserProfile).put(updateUserProfile);
-
+router
+  .route("/:id")
+  .get(protect, getUserById)
+  .delete(protect, deleteUser)
+  .put(protect, updateUser);
+router
+  .route("/profile/:id")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
