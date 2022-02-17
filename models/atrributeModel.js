f40beaccfa102a7928b5f71dae0b8872ea05affc
ngahika01 <mykenjoroge01@gmail.com>
2022-02-17 15:32:43 +0300

    Updated attribute model

--- models/atrributeModel.js
+++ models/atrributeModel.js
@@ -5,7 +5,7 @@ const atrributeSchema = mongoose.Schema(
     name: {
       type: String,
       required: true,
-      minlength: 3,
+      minlength: 1,
       maxlength: 255,
     },
     status: {
